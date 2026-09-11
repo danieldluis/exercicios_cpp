2984b7a23073bc19c5773349a6798511d403260a
danieldluis <danielpytao@gmail.com>
2026-09-11 16:36:31 +0100

    Update main.cpp

---
+++ exercicios/08-contar_pares/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Quantos numeros: ";
+    cin >> __________________;
+
+    int pares = 0, impares = 0;
+
+    for (int i = 0; i < __________________; i++) {
+        int x;
+        cin >> __________________;
+
+        // TODO: verifica se é par ou ímpar
+        if (__________________)
+            __________________;
+        else
+            __________________;
+    }
+
+    cout << "Pares: " << __________________ << endl;
+    cout << "Impares: " << __________________ << endl;
+    return 0;
+}
