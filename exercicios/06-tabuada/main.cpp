872e81f56fe71978bc0e1f5e560397bc31cbc225
danieldluis <danielpytao@gmail.com>
2026-09-11 16:34:28 +0100

    Update main.cpp

---
+++ exercicios/06-tabuada/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Numero: ";
+    cin >> __________________;
+
+    // TODO: mostra a tabuada de 1 a 10
+    // Exemplo: "3 x 1 = 3"
+    for (int i = 1; i <= 10; i++) {
+        __________________;
+    }
+
+    return 0;
+}
