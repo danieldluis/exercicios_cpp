6767b28b332277831065e604d8fa5e2759219e5a
danieldluis <danielpytao@gmail.com>
2026-09-11 16:38:29 +0100

    Update main.cpp

---
+++ solutions/02-soma/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Digite dois numeros: ";
+    cin >> a >> b;
+    cout << "Soma: " << a + b << endl;
+    return 0;
+}
