73efb6299d5290404419103ab95beb494f56a426
danieldluis <danielpytao@gmail.com>
2026-09-11 16:35:55 +0100

    Update main.cpp

---
+++ exercicios/07-fatorial/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n: ";
+    cin >> __________________;
+
+    long long fatorial = 1;
+
+    // TODO: calcula o fatorial
+    __________________;
+
+    cout << n << "! = " << __________________ << endl;
+    return 0;
+}
