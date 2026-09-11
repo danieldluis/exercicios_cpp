65ae876b2f003af760494681848dc535005ed160
danieldluis <danielpytao@gmail.com>
2026-09-11 16:37:15 +0100

    Update main.cpp

---
+++ exercicios/09-funcao_maior/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// TODO: implementa a função
+int maior(int a, int b) {
+    __________________;
+}
+
+int main() {
+    int a, b;
+    cout << "Digite dois numeros: ";
+    cin >> __________________;
+
+    cout << "Maior: " << __________________ << endl;
+    return 0;
+}
