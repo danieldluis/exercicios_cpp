9fb5406e6871390d885be17eeba537f1c883588c
danieldluis <danielpytao@gmail.com>
2026-09-11 16:38:43 +0100

    Update main.cpp

---
+++ solutions/03-area-rectangulo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double base, altura;
+    cout << "Base: ";
+    cin >> base;
+    cout << "Altura: ";
+    cin >> altura;
+    cout << "Area = " << base * altura << endl;
+    return 0;
+}
