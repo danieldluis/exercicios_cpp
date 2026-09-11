019b11ac6d0d9e73c93d9125ca7a3de8161b8861
danieldluis <danielpytao@gmail.com>
2026-09-11 16:33:11 +0100

    Update main.cpp

---
+++ exercicios/04-conversao_celsius_fahrenheit/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double c, f;
+    cout << "Temperatura em Celsius: ";
+    cin >> __________________;
+    // TODO: converte para Fahrenheit
+    f = __________________;
+    cout << __________________ << " F" << endl;
+    return 0;
+}
