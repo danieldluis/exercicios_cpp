c353122a17d8ed42eec63003aaa363bf7a96d5c7
danieldluis <danielpytao@gmail.com>
2026-09-11 16:33:56 +0100

    Update main.cpp

---
+++ exercicios/05-maior_de_3_numeros/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Digite tres numeros: ";
+    // TODO: lê os três números
+    __________________;
+
+    // TODO: mostra o maior
+    if (a > b && a > c)
+        cout << __________________ << endl;
+    else if (b > a && b > c)
+        cout << __________________ << endl;
+    else
+        cout << __________________ << endl;
+
+    return 0;
+}
