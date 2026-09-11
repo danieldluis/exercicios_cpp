c06fc0ca2676d742c51885e6282383cc69b5fc50
danieldluis <danielpytao@gmail.com>
2026-09-11 16:31:57 +0100

    Update main.cpp

---
+++ exercicios/02-soma/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Digite dois numeros: ";
+    // TODO: lê os dois números
+    __________________;
+    // TODO: mostra a soma
+    cout << "Soma: " << __________________ << endl;
+    return 0;
+}
